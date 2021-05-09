5524df4abbff49db3a6508ab8e9017fa9217f961
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-05-09 00:26:15 -0300

    Add Hooks and changing the layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,53 +15,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ComoEstas extends StatefulWidget {
-  ComoEstas({Key key}) : super(key: key);
-
-  @override
-  _ComoEstasState createState() => _ComoEstasState();
-}
-
-class _ComoEstasState extends State<ComoEstas> {
+class ComoEstas extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final counter = useState(0);
+
     return Scaffold(
-      backgroundColor: Color(0xFFe5e5e5),
+      backgroundColor: Color(0xFFEEEEEE),
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  TextNavBar(label: 'Metas', color: Color(0xFFEECD57)),
-                  SizedBox(width: 20.0),
-                  TextNavBar(label: '¿Cómo estás?', color: Color(0xFF2EC5BC)),
-                  SizedBox(width: 20.0),
-                  TextNavBar(label: 'Inspiración', color: Color(0xFFE73A97)),
-                ],
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Center(
+                child: Text(
+                  "¿Como estas?",
+                  style: TextStyle(
+                    color: Color(0xFF2EC5BC),
+                    fontSize: 36.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
               ),
+            ),
+            Row(
+              children: [
+                Container(
+                  color: Color(0xFF2EC5BC),
+                  width: 55.0,
+                  height: 90.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nueva entrada",
+                        style: TextStyle(
+                          color: Color(0xFF2EC5BC),
+                          fontSize: 24.0,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                      Text(
+                        "Tomate unos minutos para completar este día.",
+                        style: TextStyle(
+                          color: Color(0xFF2EC5BC),
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class TextNavBar extends StatelessWidget {
-  TextNavBar({@required this.label, @required this.color});
-  final Color color;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      label,
-      style: TextStyle(
-        color: color,
-        fontSize: 36.0,
-        fontFamily: 'Poppins',
       ),
     );
   }
