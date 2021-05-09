d8a5700f95c784e696a21dd431098575c88f9b68
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-05-09 01:26:41 -0300

    UI screen como_te_sentis

--- lib/screens/como_te_sentis.dart
+++ lib/screens/como_te_sentis.dart
@@ -10,7 +10,241 @@ class ComoTeSentis extends HookWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Center(
+                child: Text(
+                  "¿Como te sentis?",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Text(
+                    "Feliz",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 30.0),
+                  child: Text(
+                    "Agradecido",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 30.0),
+                  child: Text(
+                    "Ansioso",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Text(
+                    "Triste",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 30.0),
+                  child: Text(
+                    "Energetico",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Text(
+                    "Alegre",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: 60.0,
+                  height: 60.0,
+                  child: Center(
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        color: Color(0xFF2EC5BC),
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Text(
+                    "Enojado",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30.0),
+          ],
         ),
       ),
     );
